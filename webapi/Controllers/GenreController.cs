2a01b610f460d5b480fe3c52768dbe0239946498
A-Finger <anthony.palchyk@gmail.com>
2023-11-14 18:03:09 +0200

    Added checkpoint fot push

---
+++ webapi/Controllers/GenreController.cs
@@ -0,0 +1,22 @@
+﻿using Gamerize.BLL.Models;
+using Gamerize.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+	public class GenreController : ControllerBase
+	{
+		private readonly ShopService _shopService;
+
+		public GenreController(ShopService shopService)
+        {
+			_shopService = shopService;
+		}
+
+		[HttpGet("GetAll")]
+		public async Task<ActionResult<ICollection<GenreDTO>>> GetAll()
+		{
+			return Ok();
+		}
+    }
+}
