d594f4bc6ea7e15da14654ded74cd78c60c8e33b
Влад Любашенко <whatislaw007@outlook.com>
2023-11-19 17:39:56 +0200

    added my components  from the separate project

---
+++ my-app/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,32 @@
+import styles from './style/ProductCard.module.css'
+
+export default function ProductCard({configurationObject = {isOldPrice : false, isDiscount : false, isCartView : false}, product : {id,discount, name, minPlayers, maxPlayers, minAge, price, oldPrice, gameTimeMinutes,photo}}) {
+    return (
+        <div className={styles.allContent}>
+            <div className={styles.cardTop}>
+                    <div className={styles.iconsBar}>
+                        {
+                            configurationObject.isDiscount?
+                            <div className={styles.leftIcon + " " + styles.discountIcon}><p>{`-${discount}%`}</p></div>
+                            : <div className={styles.leftIcon + " " + styles.hitIcon}><p>Хіт</p></div>
+                        }
+                        <div className={styles.wishListIcon}><img src="./src/assets/images/product-card/wish-list-icon.svg" alt="wish list icon" /></div> 
+                    </div>
+                    <div className={styles.imageContainer}><img src={photo} alt={`product ${name} # ${id}`}/></div>
+                    <div className={styles.featuresBar}>
+                        <div className={styles.featuresBarElement}><p>{`${gameTimeMinutes} хв`}</p></div>
+                        <div className={styles.featuresBarElement}><p>{maxPlayers? `${minPlayers}-${maxPlayers} гравців`: `від ${minPlayers} гравців`}</p></div>
+                        <div className={styles.featuresBarElement}><p>{`${minAge}+`}</p></div>
+                    </div>
+            </div>
+            <div className={styles.cardBottom}>
+                <div className={styles.title}><h6>{name}</h6></div>
+                <div className={styles.prices}>
+                    {configurationObject.isOldPrice && <div className={styles.price + " " + styles.oldPrice}><p>{`${oldPrice}₴`}</p></div>}
+                    <div className={styles.price + " " + styles.currentPrice}><p>{`${price}₴`}</p></div>
+                </div>
+                <div className={styles.buyBtn}><p>Купити</p></div>
+            </div>
+        </div>
+    );
+}
