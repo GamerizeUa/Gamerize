46ac0c2ca056d173b21e04d2b269430109f1679a
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-11-28 18:14:58 +0200

    add category list

--- my-app/src/components/CategoryHeader/CategoryHeader.jsx
+++ my-app/src/components/CategoryHeader/CategoryHeader.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
+import Category from "./Category";
 import styles from "./CategoryHeader.module.css";
+import Genres from "./Genres";
 
 const CategoryHeader = () => {
+  const [isCategory, setIsCategory] = useState(false);
+  const [isGenre, setIsGenre] = useState(false);
+
   return (
-    <div className={styles.categoryHeader}>
-      <ul className={styles.categoryList}>
-        <li>Настільні ігри</li>
-        <li>Жанри</li>
-        <li>Тематика</li>
-        <li>Пазли</li>
-        <li>Головоломки</li>
-      </ul>
-    </div>
+    <section className={styles.categoryHeaderWrap}>
+      <div className={styles.categoryHeader}>
+        <ul className={styles.categoryList}>
+          <li
+            className={styles.categoryListItem}
+            onMouseEnter={() => {
+              setIsCategory(true);
+            }}
+            onMouseLeave={() => {
+              setIsCategory(false);
+            }}
+          >
+            Настільні ігри
+            {isCategory && <Category />}
+          </li>
+          <li
+            className={styles.categoryListItem}
+            onMouseEnter={() => {
+              setIsGenre(true);
+            }}
+            onMouseLeave={() => {
+              setIsGenre(false);
+            }}
+          >
+            Жанри
+            {isGenre && <Genres />}
+          </li>
+          <li className={styles.categoryListItem}>Тематика</li>
+          <li className={styles.categoryListItem}>Пазли</li>
+          <li className={styles.categoryListItem}>Головоломки</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- my-app/src/components/Layout/Layout.jsx
+++ my-app/src/components/Layout/Layout.jsx
@@ -1,15 +1,27 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
 import CategoryHeader from "../CategoryHeader/CategoryHeader";
+import Cart from "../Cart/Cart";
 
 const Layout = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function openCart() {
+    setCartOpen(true);
+  }
+
+  function cartClose() {
+    setCartOpen(false);
+  }
+
   return (
     <div className={styles.wrapper}>
-      <Header />
+      <Header openCart={openCart} />
       <CategoryHeader />
+      {cartOpen && <Cart cartClose={cartClose} />}
       <main className={styles.container}>
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
