9671cd553839f3bdee7dbc270dd959f1606876c9
Влад Любашенко <whatislaw007@outlook.com>
2023-12-09 21:24:27 +0200

    add base version of review

---
+++ my-app/src/components/product-page/Review/Review.jsx
@@ -0,0 +1,9 @@
+import styles from "./Review.module.css"
+
+export default function Review({feedback:{customerName, text, createdDate, rate}}) {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+}
