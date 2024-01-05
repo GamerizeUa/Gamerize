06d3c253350a58a7343dfa350cee1d33163d7493
anastasia_mia <nastia.mianovska333@gmail.com>
2024-01-05 22:03:11 +0200

    SCRUM-15: implemented pop up

---
+++ my-app/src/components/landing-page/QuestionFormPopUp/popUp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './popUp.module.css'
+
+export const PopUp = ({changeVisibility}) => {
+    return(
+        <div className={styles.popUp_background}>
+            <div className={styles.popUp}>
+                <div className={styles.popUp_container}>
+                    <div className={styles.popUp_crossContainer}>
+                        <div className={styles.popUp_cross} onClick={changeVisibility}></div>
+                    </div>
+                    <p className={styles.popUp_title}>Дякуємо за запитання!</p>
+                    <p className={styles.popUp_description}>Очікуйте на повідомлення від менеджера для вирішення вашого
+                                                            запиту!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
