88714abeba7edce68dd35f6662d9d413fcbd6ff4
Anna Opanasenko <aopanasenko@kse.org.ua>
2024-02-14 21:32:43 +0100

    migrations RefreshToken

--- Gamerize.DAL/Migrations/20240214195112_DiscountCoupon.Designer.cs
+++ Gamerize.DAL/Migrations/20240214195112_DiscountCoupon.Designer.cs
@@ -4,6 +4,7 @@ using Gamerize.DAL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gamerize.DAL.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240214195112_DiscountCoupon")]
+    partial class DiscountCoupon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gamerize.DAL/Migrations/20240214195112_DiscountCoupon.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gamerize.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DiscountCoupon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Discount",
+                table: "DiscountCoupons",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "DiscountCoupons");
+        }
+    }
+}
