c64d63f6c6607238e34c75044989445f59abb66b
Влад Любашенко <whatislaw007@outlook.com>
2024-02-23 12:02:18 +0200

    + format code, - extra containers and camelCase in classes

--- my-app/src/components/common-components/PaginationButtons/PaginationButtons.jsx
+++ my-app/src/components/common-components/PaginationButtons/PaginationButtons.jsx
@@ -1,29 +1,59 @@
 import { useState } from "react";
-import styles from "./PaginationButtons.module.css"
+import styles from "./PaginationButtons.module.css";
 import ArrowLeftIcon from "../../icons/ArrowLeftIcon";
 import ArrowRightIcon from "../../icons/ArrowRightIcon";
 
-export default function PaginationButtons({pagesAmount, pageChangeFunc}) {
-    let [currentPage, setCurrentPage] = useState(1)
-    function rightArrowClickFunc (){
-        pageChangeFunc(currentPage + 1)
-        setCurrentPage(currentPage + 1)
-    } 
-    function leftArrowClickFunc (){
-        pageChangeFunc(currentPage - 1)
-        setCurrentPage(currentPage - 1)
+export default function PaginationButtons({ pagesAmount, pageChangeFunc }) {
+    let [currentPage, setCurrentPage] = useState(1);
+    function rightArrowClickFunc() {
+        pageChangeFunc(currentPage + 1);
+        setCurrentPage(currentPage + 1);
     }
-    function notCheckedBtnClickFunc (newPage){
-        pageChangeFunc(newPage)
-        setCurrentPage(newPage)
-    } 
+    function leftArrowClickFunc() {
+        pageChangeFunc(currentPage - 1);
+        setCurrentPage(currentPage - 1);
+    }
+    function notCheckedBtnClickFunc(newPage) {
+        pageChangeFunc(newPage);
+        setCurrentPage(newPage);
+    }
     return (
         <div className={styles.container}>
-            <div onClick={leftArrowClickFunc} className={styles.chevron + " " +  (currentPage !== 1? "" : styles.hiddenChevron)}><ArrowLeftIcon color="#2B2B2B" strokeWidth="3"/></div>
-            <div className={styles.checkedBtn}><p>{currentPage}</p></div>
-            <div onClick={()=> notCheckedBtnClickFunc(currentPage + 1)} className={styles.notCheckedBtn}><p>{currentPage + 1}</p></div>
-            <div onClick={()=> notCheckedBtnClickFunc(currentPage + 2)} className={styles.notCheckedBtn}><p>{currentPage + 2}</p></div>
-            <div onClick={rightArrowClickFunc} className={styles.chevron + " " + (currentPage !== pagesAmount? "" : styles.hiddenChevron)}><ArrowRightIcon color="#2B2B2B" strokeWidth="3"/></div>
+            <div
+                onClick={leftArrowClickFunc}
+                className={
+                    styles.chevron +
+                    " " +
+                    (currentPage !== 1 ? "" : styles.hidden_chevron)
+                }
+            >
+                <ArrowLeftIcon color="#2B2B2B" strokeWidth="3" />
+            </div>
+            <div className={styles.checked_btn}>
+                <p>{currentPage}</p>
+            </div>
+            <p
+                onClick={() => notCheckedBtnClickFunc(currentPage + 1)}
+                className={styles.not_checked_btn}
+            >
+                {currentPage + 1}
+            </p>
+            <p
+                onClick={() => notCheckedBtnClickFunc(currentPage + 2)}
+                className={styles.not_checked_btn}
+            >
+                {currentPage + 2}
+            </p>
+            <div
+                onClick={rightArrowClickFunc}
+                className={
+                    styles.chevron +
+                    " " +
+                    (currentPage !== pagesAmount ? "" : styles.hidden_chevron)
+                }
+            >
+                <ArrowRightIcon color="#2B2B2B" strokeWidth="3" />
+            </div>
         </div>
     );
 }
