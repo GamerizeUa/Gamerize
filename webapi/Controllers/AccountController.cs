cd164f84f07e8181e6ceabb6da5d5fa836c945f6
DariaLopatina <151048961+DariaLopatina@users.noreply.github.com>
2024-03-07 00:20:52 +0200

    + account controller, login and register view model

--- webapi/Controllers/AccountController.cs
+++ webapi/Controllers/AccountController.cs
@@ -1,6 +1,67 @@
-﻿namespace webapi.Controllers
+﻿using Gamerize.BLL.Models;
+using Gamerize.DAL.Entities.Admin;
+
+namespace webapi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
 {
-    public class AccountController
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+
+    public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
     {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new User { UserName = model.Email, Email = model.Email };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok(new { Message = "Registration successful" });
+            }
+
+            return BadRequest(new { Errors = result.Errors });
+        }
+
+        return BadRequest(new { Message = "Invalid model state" });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { Message = "Login successful" });
+            }
+
+            return BadRequest(new { Message = "Invalid login attempt" });
+        }
+
+        return BadRequest(new { Message = "Invalid model state" });
+    }
+
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return Ok(new { Message = "Logout successful" });
     }
 }
