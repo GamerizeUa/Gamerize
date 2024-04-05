aac951179e3df07675795ac152d4acfe151f60f8
VitaliyaOvechko <113234981+VitaliyaOvechko@users.noreply.github.com>
2024-04-05 19:29:04 +0300

    add Links (#47)

--- my-app/src/components/CategoryHeader/Themes/Themes.jsx
+++ my-app/src/components/CategoryHeader/Themes/Themes.jsx
@@ -1,5 +1,6 @@
 import styles from "./Themes.module.css";
 import sprite from "../../../assets/icons/sprite.svg";
+import { Link } from "react-router-dom";
 
 const Themes = ({ themes }) => {
   return (
@@ -7,15 +8,21 @@ const Themes = ({ themes }) => {
       <ul className={styles.themesList}>
         {themes.map((theme) => (
           <li key={theme.id} className={styles.themesListItem}>
-            <div className={styles.themesIcon}>
-              <svg width="24" height="24">
-                <use
-                  href={sprite + `#icon-${theme.description}`}
-                  fill="#EEF1FF"
-                ></use>
-              </svg>
-            </div>
-            <p className={styles.themesText}>{theme.name}</p>
+            <Link
+              to="/catalog"
+              state={{ theme: theme.name }}
+              className={styles.themesListLink}
+            >
+              <div className={styles.themesIcon}>
+                <svg width="24" height="24">
+                  <use
+                    href={sprite + `#icon-${theme.description}`}
+                    fill="#EEF1FF"
+                  ></use>
+                </svg>
+              </div>
+              <p className={styles.themesText}>{theme.name}</p>
+            </Link>
           </li>
         ))}
       </ul>
