08387bc7bca911f6e7dff85c4b0193103bf5d238
anastasia_mia <nastia.mianovska333@gmail.com>
2024-04-28 15:58:04 +0300

    SCRUM-72: added login functionality

--- my-app/src/components/LoginAndRegistration/Login.jsx
+++ my-app/src/components/LoginAndRegistration/Login.jsx
@@ -1,12 +1,17 @@
 import styles from "./LoginAndRegistration.module.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Registration } from "./Registration.jsx";
 import sprite from "../../assets/icons/sprite.svg";
+import * as yup from "yup";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import Axios from "axios";
 
 export const Login = ({ setDisplayedLoginPopUp }) => {
   const [isVisible, setIsVisible] = useState(true);
   const [isDisplayedRegistrationPopUp, setIsDisplayedRegistrationPopUp] =
     useState(false);
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   useEffect(() => {
     if (isVisible) {
@@ -20,6 +25,26 @@ export const Login = ({ setDisplayedLoginPopUp }) => {
     };
   }, [isVisible]);
 
+  const schema = yup.object().shape({
+    email: yup.string().email("Введіть коректну е-пошту").required("Введіть е-пошту")
+        .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]+$/i, "Введіть коректну е-пошту"),
+    password: yup.string().required("Введіть пароль"),
+  });
+
+  const {register, handleSubmit, formState: {errors}, reset} = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) =>{
+    console.log(data)
+    Axios.post('https://gamerize.ltd.ua/api/Account/login', data)
+        .then(changeVisibility)
+        .catch((err) => {
+          setIsErrorVisible(true)
+          console.log(err)
+        })
+  }
+
   const changeVisibility = () => {
     setIsVisible(!isVisible);
   };
@@ -32,34 +57,39 @@ export const Login = ({ setDisplayedLoginPopUp }) => {
           <div className={styles.popUp_cross} onClick={changeVisibility}></div>
         </div>
         <div className={styles.popUp_container}>
-          <form className={styles.popUp_form}>
+          <form className={styles.popUp_form} onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.popUp_fromGroup}>
               <label>Електронна пошта*</label>
               <div className={styles.popUp_inputContainer}>
                 <svg width="24" height="24">
                   <use
-                    href={sprite + "#icon-email"}
-                    fill="#EEF1FF"
-                    stroke="currentColor"
+                      href={sprite + "#icon-email"}
+                      fill="#EEF1FF"
+                      stroke="currentColor"
                   ></use>
                 </svg>
-                <input type="email" />
+                <input type="email"  {...register("email")}/>
               </div>
+              <p className={styles.input_error}>{errors.email?.message}</p>
             </div>
             <div className={styles.popUp_fromGroup}>
               <label>Пароль*</label>
               <div className={styles.popUp_inputContainer}>
                 <svg width="24" height="24">
                   <use
-                    href={sprite + "#icon-lock"}
-                    fill="#EEF1FF"
-                    stroke="currentColor"
+                      href={sprite + "#icon-lock"}
+                      fill="#EEF1FF"
+                      stroke="currentColor"
                   ></use>
                 </svg>
-                <input type="password" />
+                <input type="password" {...register("password")}/>
               </div>
+              <p className={styles.input_error}>{errors.password?.message}</p>
             </div>
             <button type="submit">Увійти</button>
+            {isErrorVisible && <p className={styles.input_userError}>
+              Помилка входу. Перевірте правильність е-пошти та пароля.
+            </p>}
             <a>Забули пароль?</a>
           </form>
           <hr />
