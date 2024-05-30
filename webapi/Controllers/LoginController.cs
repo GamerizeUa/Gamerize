a4db8240a641635ef425fdf147a8b2905abb181b
Oleksandr <111017111+OleksandrHutsul@users.noreply.github.com>
2024-05-30 21:56:32 +0300

    update last commit

--- webapi/Controllers/LoginController.cs
+++ webapi/Controllers/LoginController.cs
@@ -1,5 +1,4 @@
 ﻿using Gamerize.BLL.Models;
-using Gamerize.BLL.Models.Tokens;
 using Gamerize.BLL.Services;
 using Gamerize.DAL.Entities.Admin;
 using Microsoft.AspNetCore.Authorization;
@@ -72,10 +71,16 @@ namespace webapi.Controllers
         }
 
         [HttpGet("check")]
-        [Authorize]
         public IActionResult CheckAuth()
         {
-            return Ok(new { Message = "User is authenticated" });
+            if (User.Identity.IsAuthenticated)
+            {
+                return Ok(new { Message = "User is authenticated" });
+            }
+            else
+            {
+                return Unauthorized(new { Message = "User is not authenticated" });
+            }
         }
 
         [HttpPost("forgot-password")]
@@ -98,13 +103,21 @@ namespace webapi.Controllers
             var callbackUrl = $"{frontendUrl}?email={user.Email}&token={token}";
 
             await _emailSender.SendEmailAsync(model.Email, "Скидання пароля",
-                $"Вітаємо, шановний клієнте! Ви щойно запросили дію для скидання свого паролю на сайті Gamerize! " +
-                $"\r\nПідтвердіть вашу дію, перейшовши за посиланням: " +
+                $"Шановний клієнте, для скидання паролю перейдіть за посиланням: \n" +
                 $"<a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>натисніть тут</a>.");
+
+            var resetTokenCookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddHours(1)
+            };
+
+            Response.Cookies.Append("resetToken", token, resetTokenCookieOptions);
 
             return Ok(new { Message = "Password reset link has been sent to your email.", model.Email });
         }
-
 
         [HttpPost("reset-password")]
         [AllowAnonymous]
@@ -115,13 +128,24 @@ namespace webapi.Controllers
                 return BadRequest(new { Message = "Invalid model state" });
             }
 
+            if (model.Password != model.RepeatPassword)
+            {
+                return BadRequest(new { Message = "Passwords do not match" });
+            }
+
             var user = await _userManager.FindByEmailAsync(model.Email);
             if (user == null)
             {
                 return BadRequest(new { Message = "User not found" });
             }
 
-            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+            var token = Request.Cookies["resetToken"];
+            if (string.IsNullOrEmpty(token))
+            {
+                return BadRequest(new { Message = "Invalid or expired reset token" });
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, token, model.Password);
             if (!result.Succeeded)
             {
                 return BadRequest(new { Message = "Error resetting password", Errors = result.Errors });
