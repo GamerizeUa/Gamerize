f4d051622644dcc7cba6bdcdb50c79b7b98dae1b
VitaliyaOvechko <113234981+VitaliyaOvechko@users.noreply.github.com>
2024-06-01 13:45:00 +0300

    add address form for checkout (#65)

--- my-app/src/components/Checkout/DeliveryType/AddressForm.jsx
+++ my-app/src/components/Checkout/DeliveryType/AddressForm.jsx
@@ -1,37 +1,80 @@
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import styles from "./DeliveryType.module.css";
+import { useEffect } from "react";
 
-export const AddressForm = () => {
+export const AddressForm = ({ setAddressData }) => {
+  const schema = yup.object().shape({
+    city: yup
+      .string()
+      .nullable()
+      .matches(
+        /^[A-Za-zА-Яа-я\s]+$/,
+        "Введіть коректну назву населеного пункту"
+      )
+      .required("Введіть населений пункт"),
+    address: yup
+      .string()
+      .nullable()
+      .matches(/(?=.*[A-Za-zА-Яа-я])(?=.*\d)[A-Za-zА-Яа-я\d]/, {
+        message: "Адреса повинна мати назву вулиці та номер будинку",
+        excludeEmptyString: true,
+      })
+      .required("Введіть адресу доставки"),
+  });
+
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const city = watch("city");
+  const address = watch("address");
+
+  useEffect(() => {
+    setAddressData({ city, address });
+  }, [city, address, setAddressData]);
+
   return (
     <div>
       <p className={styles.addressText}>Введіть адресу</p>
-      <form className={styles.addressForm}>
+      <div className={styles.addressForm}>
         <label htmlFor="city" className={styles.label}>
           Місто*
         </label>
-        <select id="city" name="city" className={styles.selectInput}>
-          <option value="city1">Виберіть місто</option>
-          <option value="city2">Львів</option>
-          <option value="city3">Харків</option>
-          <option value="city3">Київ</option>
-        </select>
-        <label htmlFor="street" className={styles.label}>
-          Вулиця*
+        <input
+          id="city"
+          type="text"
+          className={`${styles.addressInput} ${
+            errors.city && styles.errorInput
+          }`}
+          placeholder="Місто"
+          {...register("city")}
+        />
+        {errors?.city && (
+          <p className={styles.errorMessage}>{errors?.city.message}</p>
+        )}
+        <label htmlFor="address" className={styles.label}>
+          Адреса*
         </label>
-        <select id="street" name="street" className={styles.selectInput}>
-          <option value="street1">Виберіть вулицю</option>
-          <option value="street2">Бориспільська</option>
-          <option value="street3">Харківська</option>
-        </select>
-        <label htmlFor="building" className={styles.label}>
-          Будинок*
-        </label>
-        <select id="building" name="building" className={styles.selectInput}>
-          <option value="building1">Виберіть будинок</option>
-          <option value="building2">2</option>
-          <option value="building3">5</option>
-          <option value="building3">12</option>
-        </select>
-      </form>
+        <input
+          id="address"
+          type="text"
+          className={`${styles.addressInput} ${
+            errors.address && styles.errorInput
+          }`}
+          placeholder="Адреса"
+          {...register("address")}
+        />
+        {errors?.address && (
+          <p className={styles.errorMessage}>{errors?.address.message}</p>
+        )}
+      </div>
     </div>
   );
 };
