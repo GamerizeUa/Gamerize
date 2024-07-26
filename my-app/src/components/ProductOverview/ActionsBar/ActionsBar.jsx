2d596bb9c6c62de54f1c1bb93ee6c77303944ea6
midjiro <miha.gulak@gmail.com>
2024-07-26 13:08:29 +0300

    add outside click listener for share modal; make feedback form's textarea of fixed size; fix product detail's arrow direction

--- my-app/src/components/ProductOverview/ActionsBar/ActionsBar.jsx
+++ my-app/src/components/ProductOverview/ActionsBar/ActionsBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import ShareIcon from '../icons/ShareIcon.jsx';
 import HeartIcon from '../icons/HeartIcon.jsx';
 import { Share } from '../Share/Share.jsx';
@@ -6,51 +6,62 @@ import styles from './ActionsBar.module.css';
 
 export const ActionsBar = () => {
     const [isIconFilled, setIsIconFilled] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const shareWindowRef = useRef();
+
+    const openShareWindow = () => {
+        if (!shareWindowRef) return;
+
+        setIsOpen(true);
+        shareWindowRef.current.showModal();
+    };
+
+    const closeShareWindow = useCallback(() => {
+        if (!shareWindowRef) return;
+
+        setIsOpen(false);
+        shareWindowRef.current.close();
+    }, [shareWindowRef]);
 
     const handleResize = () => {
         setWindowWidth(window.innerWidth);
     };
 
+    const handleClickFavorite = () => {
+        setIsIconFilled((prev) => !prev);
+    };
+
     useEffect(() => {
         window.addEventListener('resize', handleResize);
+
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
 
-    const handleClickFavorite = () => {
-        setIsIconFilled((prev) => !prev);
-    };
-
-    const changeVisibility = () => {
-        setIsVisible(!isVisible);
-    };
-
     return (
         <div className={styles['action-list']}>
-            <div
+            <button
                 className={styles['action-list__item']}
                 data-filled={isIconFilled}
                 onClick={handleClickFavorite}
             >
                 <HeartIcon isFilled={isIconFilled} />
-            </div>
+            </button>
             {windowWidth > 1280 && (
-                <div
+                <button
                     className={styles['action-list__item']}
-                    onClick={changeVisibility}
+                    onClick={openShareWindow}
                 >
                     <ShareIcon />
-                </div>
+                </button>
             )}
-            {isVisible && (
-                <Share
-                    changeVisibility={changeVisibility}
-                    isVisible={isVisible}
-                />
-            )}
+            <Share
+                isOpen={isOpen}
+                onClose={closeShareWindow}
+                ref={shareWindowRef}
+            />
         </div>
     );
 };
