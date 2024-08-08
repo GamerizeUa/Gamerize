0701dfdec6d561394921ec413d75edf1ef01cef6
Vladyslav Liubashenko <vladyslav.liubashenko@playtomax.com>
2024-08-08 18:19:44 +0300

    changed pagination btns to use external state of page

--- my-app/src/components/product-page/FeedbackList/FeedbackList.jsx
+++ my-app/src/components/product-page/FeedbackList/FeedbackList.jsx
@@ -1,17 +1,18 @@
-import { useContext } from 'react';
-import { ProductContext } from '../Product';
-import { useFeedbackPagination } from '../../../hooks/useFeedbacksPagination';
-import PaginationButtons from '../../common-components/PaginationButtons/PaginationButtons';
-import Feedback from '../Feedback/Feedback';
-import styles from './FeedbackList.module.css';
+import {useContext} from "react";
+import {ProductContext} from "../Product";
+import {useFeedbackPagination} from "../../../hooks/useFeedbacksPagination";
+import PaginationButtons from "../../common-components/PaginationButtons/PaginationButtons";
+import Feedback from "../Feedback/Feedback";
+import styles from "./FeedbackList.module.css";
 
 export default function FeedbackList() {
     const feedbacksPerPage = 3;
-    const { feedbacks: reviews } = useContext(ProductContext);
+    const {feedbacks: reviews} = useContext(ProductContext);
     const {
         currentFeedbacks: currentReviews,
         totalPages: pagesAmount,
         changePage,
+        currentPage,
     } = useFeedbackPagination(reviews, feedbacksPerPage);
 
     return (
@@ -24,16 +25,17 @@ export default function FeedbackList() {
                             index !== feedbacksPerPage - 1 &&
                             index !== currentReviews.length - 1
                                 ? styles.feedback_border
-                                : ''
+                                : ""
                         }
                     >
-                        <Feedback feedback={review} />
+                        <Feedback feedback={review}/>
                     </div>
                 );
             })}
             <div className={styles.pagination_btn_group}>
                 <PaginationButtons
                     pagesAmount={pagesAmount}
+                    currentPage={currentPage}
                     pageChangeFunc={changePage}
                 />
             </div>
--- my-app/src/pages/Catalog/Catalog.jsx
+++ my-app/src/pages/Catalog/Catalog.jsx
@@ -4,97 +4,100 @@ import {CatalogFilters} from "../../components/Catalog/CatalogFilters/CatalogFil
 import {CatalogSorting} from "../../components/Catalog/CatalogSorting/CatalogSorting.jsx";
 import PaginationButtons from "../../components/common-components/PaginationButtons/PaginationButtons.jsx";
 import {Breadcrumbs} from "../../components/ProductOverview/Breadcrumbs/Breadcrumbs.jsx";
-import {setPage, setPageSize} from "../../redux/productsCatalogSlice.js";
-import {fetchProducts} from "../../redux/productsCatalogSlice.js";
+import {fetchProducts, setPage, setPageSize} from "../../redux/productsCatalogSlice.js";
 import {useDispatch, useSelector} from "react-redux";
 import ProductCardList from "../../components/common-components/ProductCardList/ProductCardList.jsx";
-import handleLinkClick from "../../helpers/ScrollToTop.js";
 import useWindowWidth from "../../components/hooks/useWindowWidth.js";
 import {CatalogMobileTabs} from "../../components/Catalog/CatalogMobileTabs/CatalogMobileTabs.jsx";
 
 const Catalog = () => {
-  const {products, totalPages, page, pageSize, loading, filters } = useSelector((state) => state.productsCatalog);
-  const dispatch = useDispatch();
-  const [chosenDisplaying, setChosenDisplaying] = useState({displayingThree: true, displayingFour: false});
-  const windowWidth = useWindowWidth();
-  const displayingThreeProductsInRow = {
-    oneLineDesktopCardsAmount: 3,
-    oneLineTabletCardsAmount: 3,
-    oneLineMobileCardsAmount: 2,
-    columnGapDesktopPercent: 1.8,
-    columnGapTabletPercent: 3.7,
-    columnGapMobilePercent: 5.5
-  }
-  const displayingFourProductsInRow = {
-    oneLineDesktopCardsAmount: 4,
-    oneLineTabletCardsAmount: 3,
-    oneLineMobileCardsAmount: 2,
-    columnGapDesktopPercent: 1.8,
-    columnGapTabletPercent: 3.7,
-    columnGapMobilePercent: 5.5
-  }
-  const [configurationObj, setConfigurationObj] = useState(displayingThreeProductsInRow)
+    const {products, totalPages, page, pageSize, loading, filters} = useSelector((state) => state.productsCatalog);
+    const dispatch = useDispatch();
+    const [chosenDisplaying, setChosenDisplaying] = useState({displayingThree: true, displayingFour: false});
+    const windowWidth = useWindowWidth();
+    const displayingThreeProductsInRow = {
+        oneLineDesktopCardsAmount: 3,
+        oneLineTabletCardsAmount: 3,
+        oneLineMobileCardsAmount: 2,
+        columnGapDesktopPercent: 1.8,
+        columnGapTabletPercent: 3.7,
+        columnGapMobilePercent: 5.5
+    };
+    const displayingFourProductsInRow = {
+        oneLineDesktopCardsAmount: 4,
+        oneLineTabletCardsAmount: 3,
+        oneLineMobileCardsAmount: 2,
+        columnGapDesktopPercent: 1.8,
+        columnGapTabletPercent: 3.7,
+        columnGapMobilePercent: 5.5
+    };
+    const [configurationObj, setConfigurationObj] = useState(displayingThreeProductsInRow);
 
-  useEffect(() => {
-    dispatch(fetchProducts({page, pageSize, filters}));
-  }, [dispatch, page, pageSize, filters]);
+    useEffect(() => {
+        dispatch(fetchProducts({page, pageSize, filters}));
+    }, [dispatch, page, pageSize, filters]);
 
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-    });
-  }, []);
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+        });
+    }, []);
 
-  useEffect(() => {
-    if(chosenDisplaying.displayingThree){
-      setConfigurationObj((prevConfig) => ({ ...prevConfig, ...displayingThreeProductsInRow }));
-      dispatch(setPageSize(12))
-    }else{
-      setConfigurationObj((prevConfig) => ({ ...prevConfig, ...displayingFourProductsInRow }));
-      dispatch(setPageSize(20))
-    }
-  }, [chosenDisplaying]);
+    useEffect(() => {
+        if (chosenDisplaying.displayingThree) {
+            setConfigurationObj((prevConfig) => ({...prevConfig, ...displayingThreeProductsInRow}));
+            dispatch(setPageSize(12));
+        } else {
+            setConfigurationObj((prevConfig) => ({...prevConfig, ...displayingFourProductsInRow}));
+            dispatch(setPageSize(20));
+        }
+    }, [chosenDisplaying]);
 
-  const changePage =  (newPage) => {
-    dispatch(setPage(newPage))
-  }
+    const changePage = (newPage) => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+        dispatch(setPage(newPage));
+    };
 
-  return(
-      <div className={styles.catalog}>
-        <div className="container">
-          <div className={styles.catalog_container}>
-            <div className={styles.catalog_breadcrumbs}>
-              <Breadcrumbs page={{name: ['Каталог'], link: ['/catalog']}} />
-            </div>
-            <div className={styles.catalog_header}>
-              <p className={styles.catalog_pageTitle}>Каталог</p>
-            </div>
-            <div className={styles.catalog_mainContainer}>
-              {windowWidth >= 1280 &&
-              <div className={styles.catalog_filters}>
-                <CatalogFilters/>
-              </div>
-              }
-              <div className={styles.catalog_displaying}>
-                {windowWidth >= 1280 ?
-                <CatalogSorting setChosenDisplaying={setChosenDisplaying} /> :
-                    <CatalogMobileTabs setChosenDisplaying={setChosenDisplaying}/>
-                }
-                {loading ? <p className={styles.catalog_empty}>Завантаження товарів ...</p> : ''}
-                <div className={styles.catalog_products}>
-                  <ProductCardList productCardList={products}
-                                   confingarationObj={configurationObj}  />
+    return (
+        <div className={styles.catalog}>
+            <div className="container">
+                <div className={styles.catalog_container}>
+                    <div className={styles.catalog_breadcrumbs}>
+                        <Breadcrumbs page={{name: ["Каталог"], link: ["/catalog"]}}/>
+                    </div>
+                    <div className={styles.catalog_header}>
+                        <p className={styles.catalog_pageTitle}>Каталог</p>
+                    </div>
+                    <div className={styles.catalog_mainContainer}>
+                        {windowWidth >= 1280 &&
+                            <div className={styles.catalog_filters}>
+                                <CatalogFilters/>
+                            </div>
+                        }
+                        <div className={styles.catalog_displaying}>
+                            {windowWidth >= 1280 ?
+                                <CatalogSorting setChosenDisplaying={setChosenDisplaying}/> :
+                                <CatalogMobileTabs setChosenDisplaying={setChosenDisplaying}/>
+                            }
+                            {loading ? <p className={styles.catalog_empty}>Завантаження товарів ...</p> : ""}
+                            <div className={styles.catalog_products}>
+                                <ProductCardList productCardList={products}
+                                                 confingarationObj={configurationObj}/>
+                            </div>
+                            {!loading && products.length === 0 ?
+                                <p className={styles.catalog_empty}>Товарів не знайдено</p> : ""}
+                        </div>
+                    </div>
+                    <div className={styles.catalog_pagination}>
+                        <PaginationButtons pagesAmount={totalPages} currentPage={page} pageChangeFunc={changePage}/>
+                    </div>
                 </div>
-                {!loading && products.length === 0 ? <p className={styles.catalog_empty}>Товарів не знайдено</p> : ''}
-              </div>
             </div>
-            <div className={styles.catalog_pagination} onClick={handleLinkClick}>
-              <PaginationButtons pagesAmount={totalPages} pageChangeFunc={changePage}/>
-            </div>
-          </div>
         </div>
-      </div>
-  )
+    );
 };
 
 export default Catalog;
