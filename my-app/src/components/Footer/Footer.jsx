8507cf1932fe42f7249588436077b00304c5a34b
midjiro <miha.gulak@gmail.com>
2024-08-23 13:37:40 +0300

    fix import issues

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,107 +1,135 @@
-import { Link } from "react-router-dom";
-import { Logo } from "../Logo/Logo";
-import sprite from "../../assets/icons/sprite.svg";
-import styles from "./Footer.module.css";
-import handleLinkClick from "../../helpers/ScrollToTop";
+import { Link } from 'react-router-dom';
+import { Logo } from '../Logo/Logo';
+import sprite from '../../assets/icons/sprite.svg';
+import styles from './Footer.module.css';
+import handleLinkClick from '../../utils/ScrollToTop';
 
 const Footer = () => {
-  return (
-    <div className={styles.footerWrap}>
-      <div className={styles.footer}>
-        <div className={styles.footerListWrapper}>
-          <Logo />
-          <ul className={styles.toUsersList}>
-            <p className={styles.footerListHeader}>Користувачам</p>
-            <li onClick={handleLinkClick}>
-              <Link to="order-pay" className={styles.footerListLink}>
-                Оплата і доставка
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="return" className={styles.footerListLink}>
-                Умови повернення
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="about" className={styles.footerListLink}>
-                Про нас
-              </Link>
-            </li>
-          </ul>
-          <ul className={styles.navList}>
-            <p className={styles.footerListHeader}>Навігація</p>
-            <li onClick={handleLinkClick}>
-              <Link to="/catalog" className={styles.footerListLink}>
-                Настільні ігри
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="/catalog" className={styles.footerListLink}>
-                Жанри
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="/catalog" className={styles.footerListLink}>
-                Тематика
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="/catalog" className={styles.footerListLink}>
-                Пазли
-              </Link>
-            </li>
-            <li onClick={handleLinkClick}>
-              <Link to="/catalog" className={styles.footerListLink}>
-                Головоломки
-              </Link>
-            </li>
-          </ul>
-          <ul className={styles.contactsList}>
-            <p className={styles.footerListHeader}>Контактна інформація</p>
-            <li>
-              <a
-                href="tel:+380987067447"
-                rel="noopener noreferrer"
-                className={styles.telLink + " " + styles.footerListLink}
-              >
-                <svg width={24} height={24}>
-                  <use
-                    href={sprite + "#icon-phone"}
-                    stroke="currentColor"
-                    fill="#AAC4FF"
-                  />
-                </svg>
-                <p>+380 98 7067 447</p>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.footerListLink}
-              >
-                <svg width={35} height={35}>
-                  <use href={sprite + "#icon-Instagram"}></use>
-                </svg>
-              </a>
-              <a
-                href="https://www.facebook.com/"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.fbIcon + " " + styles.footerListLink}
-              >
-                <svg width={35} height={35}>
-                  <use href={sprite + "#icon-Facebook"}></use>
-                </svg>
-              </a>
-            </li>
-          </ul>
+    return (
+        <div className={styles.footerWrap}>
+            <div className={styles.footer}>
+                <div className={styles.footerListWrapper}>
+                    <Logo />
+                    <ul className={styles.toUsersList}>
+                        <p className={styles.footerListHeader}>Користувачам</p>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="order-pay"
+                                className={styles.footerListLink}
+                            >
+                                Оплата і доставка
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link to="return" className={styles.footerListLink}>
+                                Умови повернення
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link to="about" className={styles.footerListLink}>
+                                Про нас
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className={styles.navList}>
+                        <p className={styles.footerListHeader}>Навігація</p>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="/catalog"
+                                className={styles.footerListLink}
+                            >
+                                Настільні ігри
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="/catalog"
+                                className={styles.footerListLink}
+                            >
+                                Жанри
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="/catalog"
+                                className={styles.footerListLink}
+                            >
+                                Тематика
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="/catalog"
+                                className={styles.footerListLink}
+                            >
+                                Пазли
+                            </Link>
+                        </li>
+                        <li onClick={handleLinkClick}>
+                            <Link
+                                to="/catalog"
+                                className={styles.footerListLink}
+                            >
+                                Головоломки
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className={styles.contactsList}>
+                        <p className={styles.footerListHeader}>
+                            Контактна інформація
+                        </p>
+                        <li>
+                            <a
+                                href="tel:+380987067447"
+                                rel="noopener noreferrer"
+                                className={
+                                    styles.telLink + ' ' + styles.footerListLink
+                                }
+                            >
+                                <svg width={24} height={24}>
+                                    <use
+                                        href={sprite + '#icon-phone'}
+                                        stroke="currentColor"
+                                        fill="#AAC4FF"
+                                    />
+                                </svg>
+                                <p>+380 98 7067 447</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.instagram.com/"
+                                target="_blank"
+                                rel="noreferrer"
+                                className={styles.footerListLink}
+                            >
+                                <svg width={35} height={35}>
+                                    <use
+                                        href={sprite + '#icon-Instagram'}
+                                    ></use>
+                                </svg>
+                            </a>
+                            <a
+                                href="https://www.facebook.com/"
+                                target="_blank"
+                                rel="noreferrer"
+                                className={
+                                    styles.fbIcon + ' ' + styles.footerListLink
+                                }
+                            >
+                                <svg width={35} height={35}>
+                                    <use href={sprite + '#icon-Facebook'}></use>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <p className={styles.rights}>
+                    © 2024 Gamerise. All Rights Reserved.
+                </p>
+            </div>
         </div>
-        <p className={styles.rights}>© 2024 Gamerise. All Rights Reserved.</p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Footer;
