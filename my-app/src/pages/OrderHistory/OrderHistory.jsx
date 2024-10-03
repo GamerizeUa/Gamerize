c1367c92aec23c0117b515930a242717e065acc9
midjiro <miha.gulak@gmail.com>
2024-10-03 18:22:42 +0300

    move pending and rejected state handlers to OrderHistory component; translate Message component props

--- my-app/src/pages/OrderHistory/OrderHistory.jsx
+++ my-app/src/pages/OrderHistory/OrderHistory.jsx
@@ -1,26 +1,80 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { OrderCounter } from './OrderCounter';
 import { OrderList } from './OrderList';
 import { OrderFilter } from './OrderFilter';
+import { selectOrdersByUser } from '@/redux/selectors.js';
+import { Message } from '@/components/Message/Message.jsx';
+import { fetchOrdersByUserId } from '@/redux/orderHistorySlice.js';
+import { getAccountInformation } from '@/utils/account';
 import styles from './OrderHistory.module.css';
 
-const OrderHistory = ({ orders }) => {
+const OrderHistoryContainer = ({ children }) => {
+    return (
+        <div className={`${styles.pageWrapper} container`}>
+            <h1 className={styles.title}>Історія замовлень</h1>
+            {children}
+        </div>
+    );
+};
+
+const OrderHistory = () => {
     const [filter, setFilter] = useState('Всі замовлення');
+    const [userId, setUserId] = useState(null);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const { orders, isLoading, error } = useSelector((state) =>
+        selectOrdersByUser(state, userId)
+    );
 
     const visibleOrders =
         filter === 'Всі замовлення'
             ? orders
             : orders.filter((order) => order.status === filter);
 
+    useEffect(() => {
+        getAccountInformation().then((user) => setUserId(user.id));
+    }, []);
+
+    useEffect(() => {
+        dispatch(fetchOrdersByUserId(userId));
+    }, [userId, dispatch]);
+
+    if (isLoading)
+        return (
+            <OrderHistoryContainer>
+                <Message
+                    title={'Завантаження...'}
+                    subtitle={
+                        'Будь ласка, зачекайте, поки ми завантажуємо ваш контент.'
+                    }
+                    btnText={'Спробувати ще раз'}
+                    handleClick={() => dispatch(fetchOrdersByUserId(userId))}
+                />
+            </OrderHistoryContainer>
+        );
+    else if (error)
+        return (
+            <OrderHistoryContainer>
+                <Message
+                    title={'Щось пішло не так'}
+                    subtitle={error ? error : 'Не вдалося завантажити контент.'}
+                    btnText={'На головну сторінку'}
+                    handleClick={() => navigate('/')}
+                />
+            </OrderHistoryContainer>
+        );
+
     return (
-        <div className={`${styles.pageWrapper} container`}>
-            <h1 className={styles.title}>Історія замовлень</h1>
+        <OrderHistoryContainer>
             <div className={styles.orderWrapper}>
                 <OrderFilter filter={filter} setFilter={setFilter} />
                 <OrderCounter ordersCount={visibleOrders.length} />
                 <OrderList orders={visibleOrders} />
             </div>
-        </div>
+        </OrderHistoryContainer>
     );
 };
 
--- my-app/src/pages/OrderHistory/OrderHistoryPage.jsx
+++ my-app/src/pages/OrderHistory/OrderHistoryPage.jsx
@@ -1,66 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchOrdersByUserId } from '@/redux/orderHistorySlice.js';
 import { PersonalOffice } from '@/components/PersonalOfficeTabs/PersonalOffice.jsx';
-import { selectOrdersByUser } from '@/redux/selectors.js';
-import { Message } from '@/components/Message/Message.jsx';
 import OrderHistory from './OrderHistory.jsx';
-import { useNavigate } from 'react-router-dom';
 
 export default function OrderHistoryPage() {
-    const [userId, setUserId] = useState(null);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const { orders, isLoading, error } = useSelector((state) =>
-        selectOrdersByUser(state, userId)
-    );
-
-    useEffect(() => {
-        const getAccountInformation = async () => {
-            const res = await axios.get(
-                'https://gamerize.ltd.ua/api/Account/profile',
-                {
-                    withCredentials: true,
-                }
-            );
-
-            setUserId(res.data.id);
-        };
-
-        getAccountInformation();
-    }, []);
-
-    useEffect(() => {
-        dispatch(fetchOrdersByUserId(userId));
-    }, [userId, dispatch]);
-
-    if (isLoading)
-        return (
-            <Message
-                title={'Loading...'}
-                subtitle={'Please wait while we load your content.'}
-                btnText={'Try again'}
-                handleClick={() => dispatch(fetchOrdersByUserId(userId))}
-            />
-        );
-    else if (orders.length === 0 || error)
-        return (
-            <Message
-                title={'Something went wrong'}
-                subtitle={
-                    error ? error : 'Unable to load content. Please try again.'
-                }
-                btnText={'Back home'}
-                handleClick={() => navigate('/')}
-            />
-        );
-
     return (
         <div>
             <PersonalOffice />
-            <OrderHistory orders={orders} userId={userId} />
+            <OrderHistory />
         </div>
     );
 }
